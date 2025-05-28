85052c841da42086a5372156524dae0d2d4b7020
Rhul27 <cafegaming4@gmail.com>
2025-05-28 20:28:09 +0530

    Create question38.py

---
+++ question38.py
@@ -0,0 +1,29 @@
+# Remove Duplicates from Sorted Array
+
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i+1
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
