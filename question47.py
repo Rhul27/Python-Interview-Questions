3d338c1dc38a57df736657e9474189e9d83ccfa9
Rhul27 <cafegaming4@gmail.com>
2025-05-31 23:08:10 +0530

    Update question47.py

--- question47.py
+++ question47.py
@@ -1,22 +1,10 @@
-# Convert Sorted Array to Binary Search Tree
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-# Input: nums = [-10,-3,0,5,9]
-# Output: [0,-3,9,-10,null,5]
-# Explanation: [0,-10,5,null,-3,null,9] 
-    
-    
-# Input: nums = [1,3]
-# Output: [3,1]
-# Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
-
-
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List, Optional
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         if not nums:
